f357227c99a1e471f9d0031575c4ab780f8aba5d
Th0masAndy <130849162+Th0masAndy@users.noreply.github.com>
2025-11-28 13:29:50 +0800

    Clean up build directories for secure-join and volepsi
    Remove temporary directories after building secure-join and volepsi.

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cd secure-join
 sed -i 's|set(GIT_TAG             "657f6da90bff5774a2d01c824e997572d5e8ba00")|set(GIT_TAG             "d21bc4d7aae941e276b92615252fd1760c902890")|' thirdparty/getLibOTe.cmake
 python3 build.py --install=../out/install -D SECUREJOIN_ENABLE_BOOST=ON -D SODIUM_MONTGOMERY=false -D ENABLE_BITPOLYMUL=false 
 cd ..
-
+rm -rf secure-join
 
 ## Build volePSI
 git clone https://github.com/ladnir/volepsi.git
@@ -21,5 +21,6 @@ sed -i 's|36cd7242e085eddba34feaa63733ec4c6ded66c7|d21bc4d7aae941e276b92615252fd
 python3 build.py --install=../out/install --sudo --system -DVOLE_PSI_ENABLE_BOOST=true -DVOLE_PSI_ENABLE_BITPOLYMUL=false -DVOLE_PSI_SODIUM_MONTGOMERY=false -DCMAKE_PREFIX_PATH=/usr/local/
 cp ./out/build/linux/volePSI/config.h ../out/install/include/volePSI
 cd ..
+rm -rf volepsi
 
 cd ..
